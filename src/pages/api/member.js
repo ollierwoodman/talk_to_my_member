afbb93f5e33ac27e8e36d82c1d00912ad1036023
DESKTOP-EIEQFRR\Ollie <owoo0002@student.monash.edu>
2023-06-21 00:46:57 +0800

    improve route response patterns

--- src/pages/api/member.js
+++ src/pages/api/member.js
@@ -4,14 +4,17 @@ import axios from 'axios';
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
       res.status(400).json('Not a GET request');
+      return
   }
 
   if (!req.query[PARAM_NAME_POSTCODE]) {
       res.status(400).json('Some or all of the query parameters were not present');
+      return
   }
 
   if (!isAustralianPostcode(req.query[PARAM_NAME_POSTCODE])) {
       res.status(400).json(`Query parameter ${QUERY_PARAM_POLICYNAME} is not a valid postcode`);
+      return
   }
 
   const postcode = req.query[PARAM_NAME_POSTCODE];
@@ -21,9 +24,13 @@ export default async function handler(req, res) {
 
   axios.get(save_our_medicare_url)
     .then((save_our_medicare_res) => {
-      res.status(200).json( save_our_medicare_res.data );
+      res.status(200)
+        .json( save_our_medicare_res.data );
+      return
     })
     .catch((err) => {
-      res.status(500).json({match: false})
+      res.status(500)
+        .json({match: false, error: err});
+      return
     })
 }
--- src/pages/api/policies.js
+++ src/pages/api/policies.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
       res.status(400).json('Not a GET request');
+      return
   }
 
   const they_vote_for_you_url = new URL(`${process.env.THEY_VOTE_FOR_YOU_API_URL}policies.json`);
@@ -12,8 +13,10 @@ export default async function handler(req, res) {
   axios.get(they_vote_for_you_url)
     .then((policies_reponse) => {
       res.status(200).json(policies_reponse.data);
+      return
     })
     .catch((err) => {
-      res.status(500).json({error: "Failed to retrieve policies"})
+      res.status(500).json({error: err});
+      return
     });
 }
